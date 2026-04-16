4fa2e8d9f7f21dcaa62f3f5749e6fd7606a3044e
webber14023 <73771426+webber14023@users.noreply.github.com>
2026-04-17 00:44:21 +0800

    Midterm Demo

---
+++ Resources/MysteryBlock.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.0" name="MysteryBlock" tilewidth="16" tileheight="16" spacing="1" tilecount="1" columns="1">
+ <image source="../../Mario Game Assets/MysteryBlock.png" width="16" height="16"/>
+</tileset>
