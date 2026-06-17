91a6e88d5b469cc030de6fefc20ebe1fe0865527
webber14023 <73771426+webber14023@users.noreply.github.com>
2026-06-17 19:25:22 +0800

    Add ptsd

---
+++ PTSD/include/config.hpp
@@ -0,0 +1,25 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include "pch.hpp" // IWYU pragma: export
+
+#include "Util/Logger.hpp"
+
+constexpr const char *TITLE = "Super Mario Bros.";
+
+constexpr int WINDOW_POS_X = SDL_WINDOWPOS_UNDEFINED;
+constexpr int WINDOW_POS_Y = SDL_WINDOWPOS_UNDEFINED;
+
+constexpr unsigned int WINDOW_WIDTH = 800;
+constexpr unsigned int WINDOW_HEIGHT = 480;
+
+constexpr Util::Logger::Level DEFAULT_LOG_LEVEL = Util::Logger::Level::DEBUG;
+
+/**
+ * @brief FPS limit
+ *
+ * Set value to 0 to turn off FPS cap
+ */
+constexpr unsigned int FPS_CAP = 60;
+
+#endif
